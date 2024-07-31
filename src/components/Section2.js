5b34e2dd9588bd6048aeb701971107f327180af4
Oliver Jones <ojones@tracware.com>
2024-07-31 16:34:55 +0100

    image, and final structure to about me

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import eodj from '../images/eodj.svg'
 
 const AboutMe = () => (
   <div id='aboutme' className="section section-2">
@@ -7,31 +8,45 @@ const AboutMe = () => (
         <div className='titleCard'>about me</div>
       </div>
       <div className='middle'>
-        <div className='cardXL'>
-          <h1>Testing</h1>
-          <img src='' alt=''></img>
+        <div className='card XL'>
+          <div className='interior'>
+            <h1>Enthusiastic and talented, I am dedicated to transforming digital ideas into reality with a creative and decisive approach.</h1>
+            <img src={eodj} alt='' title='Whoa! Look at this handsome fella!'></img>
+          </div>
         </div>
         <div className='grid'>
-          <div className='cardL'>
-            <h1>testing</h1>
+          <div className='card L'>
+            <div className='interior'>
+              <h1>testing</h1>
+            </div>
           </div>
-          <div className='cardL'>
-            <h1>testing</h1>
+          <div className='card L'>
+            <div className='interior'>
+              <h1>testing</h1>
+            </div>
           </div>
-          <div className='cardL'>
-            <h1>testing</h1>
+          <div className='card L'>
+            <div className='interior'>
+              <h1>testing</h1>
+            </div>
           </div>
-          <div className='cardL'>
-            <h1>testing</h1>
+          <div className='card L'>
+            <div className='interior'>
+              <h1>testing</h1>
+            </div>
           </div>
         </div>
       </div>
       <div className='bottom'>
-        <div className='cardS'>
-          <h1>Testing</h1>
+        <div className='card S'>
+        <div className='interior'>
+              <h1>testing</h1>
+            </div>
         </div>
-        <div className='cardLong'>
-          <h1>Testing</h1>
+        <div className='card Long'>
+        <div className='interior'>
+              <h1>testing</h1>
+            </div>
         </div>
       </div>
     </div>
