e2623ed1358d8b5e920e1e6aa1c2924502510008
Oliver Jones <ojones@tracware.com>
2024-08-18 17:55:02 +0100

    next steps

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -1,8 +1,19 @@
 import React, {useEffect} from 'react';
 import projects from '../projectdata.json';
-
+import Slider from 'react-slick';
 import externalWindow from '../images/External.svg'
 import expand from '../images/Arrow_right.svg'
+import { useState } from 'react';
+
+const sliderSettings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  arrows: true
+}
 
 const skillColorMap = {
   "HTML/CSS" : "#F13939",
@@ -27,43 +38,105 @@ const Section3 = () => {
       elements[i].style.color = color;
       elements[i].style.borderColor = color;
     }
+
+    let lastScrollY = window.scrollY;
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        setScrollDirection('down');
+      } else {
+        setScrollDirection('up');
+      }
+      lastScrollY = window.scrollY;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
+
+  const [expandedProject, setExpandedProject] = useState(null);
+  const [scrollDirection, setScrollDirection] = useState(null);
+
+
+
+  const toggleExpand = (projectId) => {
+    setExpandedProject(expandedProject === projectId ? null : projectId);
+  };
+
   
 
 
 return (
   <div id='mywork' className="section section-3">
-     <div className='inner'>
+       <div className='inner'>
         <div className="titleHeader">
-            <div className='titleCard'>my work</div>
+          <div className='titleCard'>my work</div>
         </div>
         <div className='grid'>
           {projects.map((project) => (
             <div key={project.id} className='card work'>
-            <div className='title-area'>
-              <h1>{project.title}</h1>
-              <h3>{project.jobRole}</h3>
-            </div>
-            <div className='description'>
-              <p>{project.shortline}</p>
-            </div>
-            <div className='bottomRow'>
-              <div className='grid L'>
+              <div className='title-area'>
+                <h1>{project.title}</h1>
+                <h3>{project.jobRole}</h3>
+              </div>
+              <div className='description'>
+                <p>{project.shortline}</p>
+              </div>
+              <div className='bottomRow'>
+                <div className='grid L'>
                   {project.skillsUsed.map((skill, index) => (
-                     <div key={index} className='skillset'>{skill}</div> 
+                    <div key={index} className='skillset'>{skill}</div>
                   ))}
+                </div>
+                <div className='buttonCont'
+                  role="button" 
+                  tabIndex="0"
+                  onClick={() => toggleExpand(project.id)} 
+                  onKeyDown={(e) => { if (e.key === 'Enter' || e.key === ' ') toggleExpand(project.id); }}>
+                  <a className='link' href={project.link} target='_blank' rel="noreferrer">
+                    <img className='icon' src={externalWindow} alt='link' />
+                  </a>
+                  <div className='expand' 
+                      onClick={() => toggleExpand(project.id)} 
+                      role="button" 
+                      tabIndex="0"
+                      onKeyDown={(e) => { if (e.key === 'Enter' || e.key === ' ') toggleExpand(project.id); }}>
+                    <img className='icon' src={expand} alt='expand' />
+                  </div>
+                </div>
               </div>
-              <div className='buttonCont'>
-                <a className='link' href={project.link} target='blank'><img className='icon' src={externalWindow} alt='link'/></a>
-                <a className='expand' href={project.expand}><img className='icon' src={expand} alt='expand'/></a>
+          {expandedProject === project.id && (
+            <>
+              <div className='overlay'    
+                  role="button" 
+                  tabIndex="0" 
+                  onClick={() => toggleExpand(null)}
+                  onKeyDown={(e) => { if (e.key === 'Enter' || e.key === ' ') toggleExpand(null); }}>
+                  </div>
+              <div className='expandableDiv'>
+                <Slider {...sliderSettings} className='imageSlider'>
+                  {project.images.map((image, index) => (
+                      image.url ? (
+                    <div key={index}>
+                      <img src={image.url} alt={image.alt || 'Project Image'} />
+                    </div>
+                      ) : null
+                  ))}
+                </Slider>
+                {scrollDirection === 'up' && (
+                  <div className='description-scroll'>
+                    <p>{project.description}</p>
+                  </div>
+                )}
               </div>
-            </div>
-          </div>
-          ))}
+            </>
+          )}
         </div>
-        </div>
-      </div>
-
-)};
+      ))}
+    </div>
+  </div>
+</div>
+);
+};
 
 export default Section3;
--- src/components/Section5.js
+++ src/components/Section5.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import eodj from '../images/eodj_handsome.svg'
 
 const Section5 = () => (
   <div id='contact' className="section section-5">
-    <h1>Section 5</h1>
+    <div className='center'>
+          <div className='iconImage'><img src={eodj} alt='eodj' /></div>
+          <div className='ContactInfo'>
+            <h1>Enzo Jones</h1>
+            <h3> eodjdesign@gmail.com || +44 (0) 7947 502590</h3>
+          </div>
+        </div>
   </div>
 );
 
